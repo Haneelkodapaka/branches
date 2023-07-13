97cdc9c8717e13655b7df1e73be215969272181b
Haneelkodapaka <129756018+Haneelkodapaka@users.noreply.github.com>
2023-07-13 17:27:55 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,122 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+     <h1 id='hun'>Hundai Cars</h1>
+
+     <div className='hyn'>
+      <img src='./imges/hundai.jpeg'></img>
+      <h2>Hyundai i20</h2>
+     </div>
+
+     <div className='hyn'>
+      <img src='./imges/kona.jpeg'></img>
+      <h2>Hyundai Kona</h2>
+     </div>
+
+     <div className='hyn'>
+      <img src='./imges/creta.jpeg'></img>
+      <h2>Hyundai Creta</h2>
+     </div>
+
+     <div className='hyn'>
+      <img src='./imges/venue.jpeg'></img>
+      <h2>Hyundai Venue</h2>
+      </div>
+
+
+      <h1 id='kiac'>Kia Cars</h1>
+     
+     <div className='kia'>
+      <img src='./images/kia.jpeg'></img>
+      <h2>Kia Seltos</h2>
+     </div>
+
+     <div className='kia'>
+      <img src='./images/carens.jpeg'></img>
+      <h2>Kia Carens</h2>
+     </div>
+
+     <div className='kia'>
+      <img src='./images/ev6.jpeg'></img>
+      <h2>Kia EV6</h2>
+     </div>
+
+     <div className='kia'>
+      <img src='./images/sonet.jpeg'></img>
+      <h2>Kia Sonet</h2>
+      </div>
+
+      <h1 id='msc'>Maruti Suzuki</h1>
+     
+     <div className='mar'>
+      <img src='./images/fornx.jpeg'></img>
+      <h2>Maruti Suzuki Fronx</h2>
+     </div>
+
+     <div className='mar'>
+      <img src='./images/brezza.jpeg'></img>
+      <h2>Maruti Suzuki Brezza</h2>
+     </div>
+
+     <div className='mar'>
+      <img src='./images/jimny.jpeg'></img>
+      <h2>Maruti Suzuki Jimny</h2>
+     </div>
+
+     <div className='mar'>
+      <img src='./images/dzire.jpeg'></img>
+      <h2>Maruti Suzuki Dzire</h2>
+     </div>
+
+      <h1 id='ttc'>TaTa Cars</h1>
+      
+    <div className='tat'>
+        <img src='./images/nexon.jpeg'></img>
+        <h2>TATA Nexon</h2>
+    </div>
+    
+    <div className='tat'>
+        <img src='./images/tiago.jpeg'></img>
+        <h2>TATA Tiago</h2>
+    </div>
+
+    <div className='tat'>
+        <img src='./images/altroz.jpeg'></img>
+        <h2>TATA Altroz</h2>
+    </div>
+
+    <div className='tat'>
+        <img src='./images/harrier.jpeg'></img>
+        <h2>TATA Harrier</h2>
+    </div>
+    
+      <h1 id='tot'>Toyota Cars</h1>
+
+      <div className='toy'>
+      <img src="./images/carmy.jpeg"></img>
+      <h2>Toyota Camry</h2>
+      </div>
+
+      <div className='toy'>
+      <img src="./images/vellfire.jpeg"></img>
+      <h2>Toyota Vellfire</h2>
+      </div>
+
+      <div className='toy'>
+      <img src="./images/fortuner.jpeg"></img>
+      <h2> Toyota Fortuner</h2>
+      </div>
+
+      <div className='toy'>
+      <img src="./images/glanza.jpeg"></img>
+      <h2>Toyota Glanza</h2>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
